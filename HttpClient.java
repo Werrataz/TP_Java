dc66c5cf2b3a30265e706964dbcd97697b2ed033
Antoine <antoine.haon@grenoble-em.com>
2024-10-16 14:45:00 +0200

    first commit

---
+++ HttpClient.java
@@ -0,0 +1,18 @@
+public class HttpClient {
+
+    String protocol, host, filename;
+    int port;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    void readURL(String url) {
+        // read the content of the URL
+    }
+
+    String getURL(String url) {
+        // return the content of the URL
+        return null;
+    }
+}
