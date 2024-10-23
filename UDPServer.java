bc74c4fdbde02363d84b97b8a100b0e9e5ab2a1c
Antoine <antoine.haon@grenoble-em.com>
2024-10-23 13:58:43 +0200

    2 Constructors instanciated

---
+++ UDPServer.java
@@ -0,0 +1,13 @@
+public class UDPServer {
+
+    int port;
+    
+    public UDPServer(int port) {
+        this.port = port;
+    }
+
+    public UDPServer() {
+        this.port = 8080;
+    }
+
+}
