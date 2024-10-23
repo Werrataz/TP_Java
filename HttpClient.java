c87ed3319e0b2b2d76ae769e860eb21afba65202
Antoine <antoine.haon@grenoble-em.com>
2024-10-23 13:47:17 +0200

    Last version of TP1

--- HttpClient.java
+++ HttpClient.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
@@ -22,13 +17,19 @@ public class HttpClient {
         client.port = 80;
         client.filename = "/rfc.txt";
         client.readURL(args[0]);
+        client.getURL();
     }
 
     void readURL(String in) {
         try {
             URL url = new URL(in);
             this.host = url.getHost();
-            this.port = url.getPort();
+            if (url.getPort() >= 0 && url.getPort() <= 49151) {
+                this.port = url.getPort();
+            }
+            if(url.getFile().length() > 0) {
+                this.filename = url.getFile();
+            }
             this.filename = url.getFile();
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -40,7 +41,7 @@ public class HttpClient {
             Socket socket = new Socket(this.host, this.port);
             int compt = 0;
             while (!socket.isConnected()) {
-                if (compt > 50) { // Interupt after 5 seconds
+                if (compt > 50) { // Interuption after 5 seconds
                     System.err.println("Connection failed");
                     System.exit(1);
                 }
@@ -52,10 +53,22 @@ public class HttpClient {
                 compt++;
             }
             System.out.println("Connected to " + this.host + " on port " + this.port);
-            InputStream inputStream = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            OutputStream outputStream = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(outputStream, true);
+            
+            // InputStream inputStream = socket.getInputStream();
+            // BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            // PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            // writer.println("GET " + this.filename + " HTTP/1.1");
+            // writer.println("Host: " + this.host);
+
+            // OutputStream out = new FileOutputStream(this.filename);
+            // InputStream from = socket.getInputStream();
+            // byte[] buffer = new byte[4096];
+            // int byte_read;
+            // while ((byte_read = from.read(buffer)) != -1) {
+            //     out.write(buffer, 0, byte_read);
+            // }
+            socket.close();
+            // out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
