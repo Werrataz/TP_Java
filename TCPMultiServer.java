14aaf586f07ec041afe94038a61bb41940ba7505
Antoine <antoine.haon@grenoble-em.com>
2024-11-27 14:02:24 +0100

    savetep

---
+++ TCPMultiServer.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+   
+public class TCPMultiServer {
+
+    private int port;
+
+    public TCPMultiServer(int port) {
+        if (port < 0 || port > 49151) {
+            System.err.println("Port number must be between 0 and 49151");
+            this.port = 8080;
+        } else {
+            this.port = port;
+        }
+    }
+
+    public TCPMultiServer() {
+        this.port = 8080;
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(this.port)) {
+            System.out.println("Server is listening on port " + this.port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                // Create a new thread for each client connection
+                new ClientHandler(socket).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // class to manage each client connection
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+
+                String clientMessage;
+                while ((clientMessage = in.readLine()) != null) {
+                    // We write a message with the ip address to identify each client
+                    System.out.println("Received from client " + socket.getInetAddress().getHostAddress() + ", Message : " + clientMessage);
+                    if ("exit".equalsIgnoreCase(clientMessage)) {
+                        System.out.println("Client disconnected");
+                        break;
+                    }
+                    out.println("Server received: " + clientMessage);
+                }
+                System.out.println("Closing the connection");
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TCPMultiServer server;
+        if (args.length < 1) {
+            server = new TCPMultiServer();
+        } else {
+            int port = Integer.parseInt(args[0]);
+            server = new TCPMultiServer(port);
+        }
+        server.start();
+    }
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
-# Projet Java dans le cadre du cursus à l'ENSEA
+# Java project as part of ENSEA curriculum       
    
+<<<<<<< HEAD
 Cette partie du code a été réalisée pendant le TP 2 et 3. Il s'agit de la partie pour le **les clients**.    
 La branche server contient le code des différents serveurs capables de communiquer avec leur client respectif. 
+=======
+This part of the code was created during TP 2 & 3. It is the part for the **client**.      
+The __server__ branch contains the differents clients capable of communicating with the corresponding server in this branch.    
+>>>>>>> server
