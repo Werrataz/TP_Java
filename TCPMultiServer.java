6e7cd2d5013499aa64650b358e065c3066e26457
Antoine <antoine.haon@grenoble-em.com>
2024-12-04 16:46:32 +0100

    commit after finished all the 7th laayer

--- TCPMultiServer.java
+++ TCPMultiServer.java
@@ -6,11 +6,19 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * TCPMultiServer is a multi-client TCP server that handles multiple clients concurrently.
+ */
 public class TCPMultiServer {
 
     private int port;
     private Map<String, ClientHandler> clients;
 
+    /**
+     * Constructs a TCPMultiServer with the specified port.
+     * 
+     * @param port The port number on which the server will listen.
+     */
     public TCPMultiServer(int port) {
         if (port < 0 || port > 49151) {
             System.err.println("Port number must be between 0 and 49151");
@@ -21,16 +29,23 @@ public class TCPMultiServer {
         this.clients = new HashMap<>();
     }
 
+    /**
+     * Constructs a TCPMultiServer with the default port 8080.
+     */
     public TCPMultiServer() {
         this.port = 8080;
         this.clients = new HashMap<>();
     }
 
+    /**
+     * Starts the TCP server and listens for client connections.
+     */
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(this.port)) {
             System.out.println("Server is listening on port " + this.port);
 
             while (true) {
+                // Accept a new client connection
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
                 ClientHandler clientHandler = new ClientHandler(socket);
@@ -42,6 +57,9 @@ public class TCPMultiServer {
         }
     }
 
+    /**
+     * ClientHandler is a thread that handles communication with a single client.
+     */
     private class ClientHandler extends Thread {
         private Socket socket;
         private String nickname;
@@ -53,21 +71,36 @@ public class TCPMultiServer {
         private int numberOfBlockedMessages;
         private Boolean banned;
         protected static Map<String, ClientHandler> clients = new HashMap<>();
-        
 
+        /**
+         * Constructs a ClientHandler for the specified socket.
+         * 
+         * @param socket The socket connected to the client.
+         */
         public ClientHandler(Socket socket) {
             this.socket = socket;
         }
 
+        /**
+         * Runs the client handler thread, handling communication with the client.
+         */
         public void run() {
             try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
                 this.printOut = out;
                 this.readIn = in;
                 ChatProtocolParser parser = new ChatProtocolParser();
                 String clientMessage;
                 
-                this.nickname = socket.getInetAddress().getHostAddress();
+                String nickname = socket.getInetAddress().getHostAddress();
+                int i = 1;
+                for (ClientHandler client : ClientHandler.clients.values()) {
+                    if (client.nickname.equals(nickname)) {
+                        nickname = String.format("%s-%d", nickname, i);
+                        i++;
+                    }
+                }
+                this.nickname = nickname;
                 this.banned = false;
                 ClientHandler.clients.put(this.nickname, this);
                 
@@ -83,28 +116,53 @@ public class TCPMultiServer {
                             out.println("Message from " + this.nickname + " was blocked");
                         } else {
                             System.out.println("Received from client " + this.nickname + ", Message: " + clientMessage);
-                            out.println("message received");
+                            for (ClientHandler client : ClientHandler.clients.values()) {
+                                if (!client.nickname.equals(this.nickname)) {
+                                    client.sendMessage("Message from " + this.nickname + ": " + clientMessage);
+                                } else {
+                                    out.println("Message sent to all");
+                                }
+                            }
                         }
                     } else if (parsedMessage.type == ChatProtocolParser.CommandType.NICKNAME) {
-                        System.out.println("User: " + this.nickname + " will be renamed to " + parsedMessage.value);
+                        String newNickname = parsedMessage.value;
+                        if (this.nickname.equals(newNickname)) {
+                            out.println("You are already known as " + this.nickname);
+                            continue;
+                        }
+                        int j = 1;
+                        Boolean exists = false;
+                        int secure = 0;
+                        while(ClientHandler.clients.containsKey(newNickname)) {
+                            newNickname = String.format("%s%d", newNickname, j);
+                            j++;
+                            exists = true;
+                            if (secure > 100) {
+                                newNickname = newNickname + java.util.UUID.randomUUID().toString();
+                                break;
+                            }
+                        }
                         ClientHandler.clients.remove(this.nickname);
-                        this.nickname = parsedMessage.value;
+                        this.nickname = newNickname;
                         ClientHandler.clients.put(this.nickname, this);
-                        out.println("Successfully changed nickname to " + this.nickname);
-                        
+                        if (exists) {
+                            out.println("User '" + parsedMessage.value + "' already exists, renamed to " + this.nickname);
+                        } else {
+                            out.println("Successfully changed nickname to " + this.nickname);
+                        }
                     } else if (parsedMessage.type == ChatProtocolParser.CommandType.BAN) {
                         this.banned = true;
                         System.out.println("User: " + this.nickname + " is now banned");
                         out.println("User: " + this.nickname + " is now banned");
-
+                        //                         out.println("We want to defend the freedom of speech, so you can't ban or be banned (we didn't have the time to implement it)");
                     } else if (parsedMessage.type == ChatProtocolParser.CommandType.STAT) {
-                        out.println("User: " + this.nickname + ", Number of messages: " + this.numberOfMessages + ", Number of blocked messages: " + this.numberOfBlockedMessages + ", pourcentage of blocked messages: " + (this.numberOfBlockedMessages / this.numberOfMessages) * 100 + "%");
+                        out.println("User: " + this.nickname + ", Number of messages: " + this.numberOfMessages + ", Number of blocked messages: " + this.numberOfBlockedMessages + ", pourcentage of blocked messages: " + {this.numberOfMessages ? (this.numberOfBlockedMessages / this.numberOfMessages * 100) : 0} + "%");
                     } else if (parsedMessage.type == ChatProtocolParser.CommandType.MENTION) {
                         String mentionedUser = parsedMessage.value;
                         ClientHandler mentionedClient = ClientHandler.clients.get(mentionedUser);
                         System.out.println(mentionedClient);
                         if (mentionedClient != null) {
-                            if(mentionedClient.sendMessage("Message from " + this.nickname + " : " + clientMessage)) {
+                            if(mentionedClient.sendMessage("Message from " + this.nickname + ": " + clientMessage)) {
                                 out.println("Message sent to " + mentionedUser);
                             } else {
                                 out.println("Error, " + mentionedUser + " is not reachable");
@@ -122,6 +180,12 @@ public class TCPMultiServer {
             }
         }
 
+        /**
+         * Sends a message to the client.
+         * 
+         * @param message The message to send.
+         * @return true if the message was sent successfully, false otherwise.
+         */
         private Boolean sendMessage(String message) {
             try {
                 this.printOut.println(message);
@@ -132,6 +196,11 @@ public class TCPMultiServer {
         }
     }
 
+    /**
+     * The main method to start the TCPMultiServer.
+     * 
+     * @param args Command line arguments, where the first argument can be the server's port number (and others are ignored).
+     */
     public static void main(String[] args) {
         TCPMultiServer server;
         if (args.length < 1) {
--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
 # Java project as part of ENSEA curriculum       
-  
    
 This part of the code was created during TP 4. You can see the client and server part here. The TCPMultiServer can know respond to the clients. We implemented many features for the layer 7 (MENTION to send messages to a specific user for example).
